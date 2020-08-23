513b0da1895c244278ea3033141e6bb619418db2
piokozi <47647338+PioKozi@users.noreply.github.com>
2020-08-23 12:42:56 +0100

    Exercise 35: C++ formatting

--- 35 - Picking a Winner/C++/main.cpp
+++ 35 - Picking a Winner/C++/main.cpp
@@ -18,17 +18,18 @@ std::vector<std::string> generate_list(std::string output)
     }
 }
 
-int random_int(int low, int high ) {
-    std::random_device rd; // random number from hardware
-    std::mt19937 generate(rd()); // seed generator
-    std::uniform_int_distribution<> distrubute(low, high); // define the range
+int random_int(int low, int high)
+{
+    std::random_device              rd;  // random number from hardware
+    std::mt19937                    generate(rd());         // seed generator
+    std::uniform_int_distribution<> distrubute(low, high);  // define the range
     return distrubute(generate);
 }
 
 int main()
 {
-    std::vector<std::string> names = generate_list("Enter a name: ");
-    int random_index = random_int(0, names.size()-1);
+    std::vector<std::string> names        = generate_list("Enter a name: ");
+    int                      random_index = random_int(0, names.size() - 1);
     std::cout << "The winner is... " << names[random_index] << std::endl;
     return 0;
 }
